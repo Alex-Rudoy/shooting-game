073e5d68cb7db9ef497ec2f496d06c9c722fec4d
Alex-Rudoy <rudoy4ik@gmail.com>
2020-09-16 23:50:13 +0300

    move drawing logic to module, player rotation

--- app/js/index.js
+++ app/js/index.js
@@ -1,20 +1,15 @@
 import "../scss/main.scss";
-import images from "./classes/images";
-
-let canvas = document.querySelector(".game");
-/** @type {CanvasRenderingContext2D} */
-let ctx = canvas.getContext("2d");
+import draw from "./logic/draw";
 
 let f = 0;
 let prevTime = 0;
 
-function draw(time) {
-  requestAnimationFrame(draw);
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+function gameLoop(time) {
+  requestAnimationFrame(gameLoop);
   f = time - prevTime;
   prevTime = time;
 
-  ctx.drawImage(images.player, 100, 100);
+  draw(f);
 }
 
-draw();
+gameLoop();
