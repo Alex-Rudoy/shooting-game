bd59cd3e6f7727bf4488d10c0e2487c2d291e3b5
Alex-Rudoy <rudoy4ik@gmail.com>
2020-09-16 18:59:10 +0300

    time variables and test draw

--- app/js/index.js
+++ app/js/index.js
@@ -1,10 +1,20 @@
 import "../scss/main.scss";
+import images from "./classes/images";
 
 let canvas = document.querySelector(".game");
 /** @type {CanvasRenderingContext2D} */
 let ctx = canvas.getContext("2d");
 
+let f = 0;
+let prevTime = 0;
+
 function draw(time) {
   requestAnimationFrame(draw);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  f = time - prevTime;
+  prevTime = time;
+
+  ctx.drawImage(images.player, 100, 100);
 }
+
 draw();
