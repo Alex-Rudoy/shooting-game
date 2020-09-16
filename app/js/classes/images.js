f25bb3a52e332de83dd2506e50e68cc263e7ba98
Alex-Rudoy <rudoy4ik@gmail.com>
2020-09-16 18:58:29 +0300

    images for start

---
+++ app/js/classes/images.js
@@ -0,0 +1,18 @@
+let images = {};
+
+images.player = new Image();
+images.player.src = "../img/player.png";
+
+images.playerShot = new Image();
+images.playerShot.src = "../img/playerShot.png";
+
+images.enemy_1_0 = new Image();
+images.enemy_1_0.src = "../img/enemy_1_0.png";
+
+images.enemy_2_0 = new Image();
+images.enemy_2_0.src = "../img/enemy_2_0.png";
+
+images.enemy_3_0 = new Image();
+images.enemy_3_0.src = "../img/enemy_3_0.png";
+
+export default images;
