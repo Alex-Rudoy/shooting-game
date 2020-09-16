88908412d5d6e4425b9dd7e0f5f834c6d7261b23
Alex-Rudoy <rudoy4ik@gmail.com>
2020-09-16 18:15:19 +0300

    basic setup

--- app/js/index.js
+++ app/js/index.js
@@ -1,5 +1,9 @@
 import "../scss/main.scss";
 
+let canvas = document.querySelector(".game");
+/** @type {CanvasRenderingContext2D} */
+let ctx = canvas.getContext("2d");
+
 function draw(time) {
   requestAnimationFrame(draw);
 }
