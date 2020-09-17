9eb97b4158929c5ab974ad1b6b8aefca6b7a489a
Alex-Rudoy <rudoy4ik@gmail.com>
2020-09-17 13:32:59 +0300

    player controls to separate file

--- app/js/classes/player.js
+++ app/js/classes/player.js
@@ -1,5 +1,5 @@
-import Entity from "./entity";
-import images from "./images";
+import images from "../logic/images";
+import Entity from "./Entity";
 
 export default class Player extends Entity {
   constructor(...args) {
@@ -8,7 +8,7 @@ export default class Player extends Entity {
     this.x = 800;
     this.y = 450;
     this.image = images.player;
-    this.hitbox = 40;
+    this.hitbox = 20;
 
     this.turnAngle = 0;
 
@@ -20,60 +20,5 @@ export default class Player extends Entity {
     this.moveLeft = false;
     this.moveBot = false;
     this.moveRight = false;
-
-    this.controls();
-    this.playerRotation();
-  }
-
-  playerRotation() {
-    let canvas = document.querySelector(".game");
-
-    canvas.onmousemove = (event) => {
-      this.turnAngle = Math.atan2(
-        ((event.clientY - canvas.offsetTop) / canvas.clientHeight) * canvas.height - this.y,
-        ((event.clientX - canvas.offsetLeft) / canvas.clientWidth) * canvas.width - this.x
-      );
-    };
-  }
-
-  controls() {
-    document.addEventListener("keydown", (e) => this.keyDownHandler(e));
-    document.addEventListener("keyup", (e) => this.keyUpHandler(e));
-  }
-
-  keyDownHandler(event) {
-    if (event.code == "KeyW") this.moveTop = true;
-    if (event.code == "KeyA") this.moveLeft = true;
-    if (event.code == "KeyS") this.moveBot = true;
-    if (event.code == "KeyD") this.moveRight = true;
-    this.recalculateMoveAngle();
-  }
-
-  keyUpHandler(event) {
-    if (event.code == "KeyW") this.moveTop = false;
-    if (event.code == "KeyA") this.moveLeft = false;
-    if (event.code == "KeyS") this.moveBot = false;
-    if (event.code == "KeyD") this.moveRight = false;
-    this.recalculateMoveAngle();
-  }
-
-  recalculateMoveAngle() {
-    if (this.moveTop || this.moveLeft || this.moveRight || this.moveBot) {
-      this.speed = this.maxSpeed;
-    } else {
-      this.speed = 0;
-    }
-
-    if (this.moveTop && !this.moveBot) {
-      this.moveAngle = -Math.PI / 2 - (Math.PI / 4) * this.moveLeft + (Math.PI / 4) * this.moveRight;
-    }
-
-    if (this.moveBot && !this.moveTop) {
-      this.moveAngle = Math.PI / 2 + (Math.PI / 4) * this.moveLeft - (Math.PI / 4) * this.moveRight;
-    }
-
-    if (this.moveTop == this.moveBot) {
-      this.moveAngle = Math.PI * this.moveLeft + Math.PI * this.moveLeft * this.moveRight;
-    }
   }
 }
---
+++ app/js/logic/playerControls.js
@@ -0,0 +1,52 @@
+export default function playerControls({ player, playerShots }, f) {
+  let canvas = document.querySelector(".game");
+
+  // player rotation
+  canvas.onmousemove = (event) => {
+    player.turnAngle = Math.atan2(
+      ((event.clientY - canvas.offsetTop) / canvas.clientHeight) * canvas.height - player.y,
+      ((event.clientX - canvas.offsetLeft) / canvas.clientWidth) * canvas.width - player.x
+    );
+  };
+
+  // player movement
+  document.addEventListener("keydown", (e) => keyDownHandler(e));
+  document.addEventListener("keyup", (e) => keyUpHandler(e));
+
+  function keyDownHandler(e) {
+    if (e.code == "KeyW") player.moveTop = true;
+    if (e.code == "KeyA") player.moveLeft = true;
+    if (e.code == "KeyS") player.moveBot = true;
+    if (e.code == "KeyD") player.moveRight = true;
+    recalculateMoveAngle();
+  }
+
+  function keyUpHandler(e) {
+    if (e.code == "KeyW") player.moveTop = false;
+    if (e.code == "KeyA") player.moveLeft = false;
+    if (e.code == "KeyS") player.moveBot = false;
+    if (e.code == "KeyD") player.moveRight = false;
+    recalculateMoveAngle();
+  }
+
+  function recalculateMoveAngle() {
+    if (player.moveTop || player.moveLeft || player.moveRight || player.moveBot) {
+      player.speed = player.maxSpeed;
+    } else {
+      player.speed = 0;
+    }
+
+    if (player.moveTop && !player.moveBot) {
+      player.moveAngle = -Math.PI / 2 - (Math.PI / 4) * player.moveLeft + (Math.PI / 4) * player.moveRight;
+    }
+
+    if (player.moveBot && !player.moveTop) {
+      player.moveAngle = Math.PI / 2 + (Math.PI / 4) * player.moveLeft - (Math.PI / 4) * player.moveRight;
+    }
+
+    if (player.moveTop == player.moveBot) {
+      player.moveAngle = Math.PI * player.moveLeft + Math.PI * player.moveLeft * player.moveRight;
+    }
+  }
+
+}
