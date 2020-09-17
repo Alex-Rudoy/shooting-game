39b4db0bc7257bde4b0f5ef2a9c94aedd7a8f683
Alex-Rudoy <rudoy4ik@gmail.com>
2020-09-17 20:40:17 +0300

    circle chaotic decision logic

--- app/js/classes/EnemyCircle1.js
+++ app/js/classes/EnemyCircle1.js
@@ -9,6 +9,28 @@ export default class EnemyCircle1 extends Enemy {
 
     this.hitbox = 20;
     this.turnAngle = 0;
-    this.speed = 2;
+    this.maxSpeed = 3;
+    this.speed = 3;
+
+    this.maxHP = 1;
+    this.HP = 1;
+
+    this.state = "chooseDirection";
   }
 }
+
+EnemyCircle1.prototype.decision = function (player) {
+  let [playerDistance, playerAngle] = this.getPlayerPosition(player);
+  if (this.state == "chooseDirection") {
+    this.turnAngle = playerAngle - Math.PI / 3 + (Math.random() * Math.PI * 2) / 3;
+    this.state = "move";
+    setTimeout(() => {
+      this.state = "wait";
+      this.speed = 0;
+      setTimeout(() => {
+        this.state = "chooseDirection";
+        this.speed = this.maxSpeed;
+      }, 500);
+    }, 1000);
+  }
+};
