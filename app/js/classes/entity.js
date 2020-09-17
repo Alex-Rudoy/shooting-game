90a7c4555d3464e50221b0a37f76afa71a501a94
Alex-Rudoy <rudoy4ik@gmail.com>
2020-09-17 13:47:30 +0300

    changed entity.draw function to use object props

--- app/js/classes/entity.js
+++ app/js/classes/entity.js
@@ -13,9 +13,9 @@ Entity.prototype.move = function (angle, distance) {
 /** @type {CanvasRenderingContext2D} */
 let ctx = document.querySelector(".game").getContext("2d");
 
-Entity.prototype.draw = function (img, x, y, angle) {
-  ctx.setTransform(1, 0, 0, 1, x, y); // set position of image center
-  ctx.rotate(angle); // rotate
-  ctx.drawImage(img, -img.width / 2, -img.height / 2); // draw image offset so its center is at x,y
+Entity.prototype.draw = function () {
+  ctx.setTransform(1, 0, 0, 1, this.x, this.y); // set position of image center
+  ctx.rotate(this.turnAngle); // rotate
+  ctx.drawImage(this.image, -this.image.width / 2, -this.image.height / 2); // draw image offset so its center is at x,y
   ctx.setTransform(1, 0, 0, 1, 0, 0); // restore default transform
 };
