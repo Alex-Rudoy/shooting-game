a999ee83838cd8818fa3f2b9d890d3f007d1e686
Alex-Rudoy <rudoy4ik@gmail.com>
2020-09-17 14:16:39 +0300

    also renamed in images object

--- app/js/logic/images.js
+++ app/js/logic/images.js
@@ -6,13 +6,13 @@ images.player.src = "../img/player.png";
 images.playerShot = new Image();
 images.playerShot.src = "../img/playerShot.png";
 
-images.enemy_1_0 = new Image();
-images.enemy_1_0.src = "../img/enemy_1_0.png";
+images.EnemyRectangle1 = new Image();
+images.EnemyRectangle1.src = "../img/EnemyRectangle1.png";
 
-images.enemy_2_0 = new Image();
-images.enemy_2_0.src = "../img/enemy_2_0.png";
+images.EnemyCircle1 = new Image();
+images.EnemyCircle1.src = "../img/EnemyCircle1.png";
 
-images.enemy_3_0 = new Image();
-images.enemy_3_0.src = "../img/enemy_3_0.png";
+images.EnemyHexagon1 = new Image();
+images.EnemyHexagon1.src = "../img/EnemyHexagon1.png";
 
 export default images;
