bee684b8700b316b9d3e46cc9ce6c2d199cafd6a
Alex-Rudoy <rudoy4ik@gmail.com>
2020-09-18 23:36:45 +0300

    toMenu and restart logic, levelStart optimization

--- app/js/classes/GameStateManager.js
+++ app/js/classes/GameStateManager.js
@@ -27,6 +27,7 @@ export default class GameStateManager {
     this.buttons.forEach((button) => {
       button.addEventListener("click", (e) => this.buttonClickHandler(e));
     });
+    this.toMenu();
   }
 
   loop(f) {
@@ -59,13 +60,19 @@ export default class GameStateManager {
     let buttonClicked = e.target.dataset.button;
     switch (buttonClicked) {
       case "Tutorial":
-        this.startTutorial();
+        this.startLevel(0);
         return;
       case "Level":
         this.startLevel(e.target.dataset.level);
         return;
       case "Continue":
         this.pause();
+        return;
+      case "Restart":
+        this.startLevel(this.lvl);
+        return;
+      case "Menu":
+        this.toMenu();
         return;
       default:
         return;
@@ -82,8 +89,29 @@ export default class GameStateManager {
     if (this.maxlvl >= lvl) {
       this.screen = "game";
       this.lvl = lvl;
+      this.wave = 0;
+      this.playerShots = [];
+      this.enemies = [];
+      this.waveTime = 0;
+      this.getWaveEnemies();
+
+      this.player.x = 800;
+      this.player.y = 450;
+      this.player.HP = 3;
+
+      this.canvas.classList.remove("blurred");
       this.menuScreen.classList.remove("menu--visible");
+      this.pauseScreen.classList.remove("pause--visible");
+      this.gameOverScreen.classList.remove("game-over--visible");
     }
+  }
+
+  toMenu() {
+    this.screen = "menu";
+    this.menuScreen.classList.add("menu--visible");
+    this.pauseScreen.classList.remove("pause--visible");
+    this.gameOverScreen.classList.remove("game-over--visible");
+    this.canvas.getContext("2d").clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
 
   gameOver() {
